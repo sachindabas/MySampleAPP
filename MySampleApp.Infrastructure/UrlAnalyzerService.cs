8061a790a461a7229cf6a5a97ab07f9264101ee1
Sachin Kumar Dabas <sachin.kumar.dabas@accenture.com>
2023-07-11 05:19:43 +0000

    Updated code to add chromew driver for word count

--- MySampleApp.Infrastructure/UrlAnalyzerService.cs
+++ MySampleApp.Infrastructure/UrlAnalyzerService.cs
@@ -1,15 +1,8 @@
 ﻿using MySampleApp.Core.Entities;
 using MySampleApp.Core.Interfaces;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
+using OpenQA.Selenium.Chrome;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using System.Xml;
-using static System.Net.Mime.MediaTypeNames;
-using HtmlAgilityPack;
-using ScrapySharp.Extensions;
-using ScrapySharp.Network;
+
 namespace MySampleApp.Infrastructure.Services
 {
     public class UrlAnalyzerService : IUrlAnalyzer
@@ -19,7 +12,7 @@ namespace MySampleApp.Infrastructure.Services
         /// We can also use some 3rd party library
         /// Need to explore further
         /// </summary>
-        string[] stopWords = new string[] { "the", "of", "and", "a", "an", "in", "to", "for", "is", "it", "on", "with", "as", "at", "by" };
+        string[] stopWords = new string[] { "the", "of", "and", "a", "an", "in", "to", "for", "is", "it", "on", "with", "as", "at", "by","you","your" };
         public async Task<List<MySampleApp.Core.Entities.ImageExt>> GetImagesFromUrl(string url)
         {
             using (var httpClient = new HttpClient())
@@ -36,39 +29,59 @@ namespace MySampleApp.Infrastructure.Services
         }
         public async Task<WordCountResult> GetWordCount(string url)
         {
-            var browser = new ScrapingBrowser();
-            var page = await browser.NavigateToPageAsync(new Uri(url));
-
-            var textNodes = page.Html
-                .DescendantsAndSelf()
-                .Where(node => node.NodeType == HtmlNodeType.Text && !string.IsNullOrWhiteSpace(node.InnerText) && node.ParentNode.Name != "script");
-
-            var wordCount = new Dictionary<string, int>();
-            foreach (var node in textNodes)
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments("--headless"); // Run Chrome in headless mode
+                                                      // Provide the path to the ChromeDriver executable
+            var chromeDriverPath = @"..\"; // Update with the actual path
+            using (var driver = new ChromeDriver(chromeDriverPath, chromeOptions))
             {
-                string[] words = Regex.Matches(node.InnerText, @"\b(?!(?:\d+\.?)+\b)\w+\b")
+                driver.Navigate().GoToUrl(url);
+                await Task.Delay(2000); // Delay to allow JavaScript to execute (adjust as needed)
+
+                var textContent = driver.FindElement(OpenQA.Selenium.By.TagName("body")).Text;
+
+                var words = Regex.Matches(textContent, @"\b(?!(?:\d+\.?)+\b)\w+\b")
                     .Cast<Match>()
                     .Select(m => m.Value.ToLower().Trim())
-                    .Where(word => !string.IsNullOrEmpty(word) && !stopWords.Contains(word) && !word.Equals("width"))
+                    .Where(word =>
+                        !string.IsNullOrEmpty(word) &&
+                        !stopWords.Contains(word) &&
+                        !IsCssRelated(word) &&
+                        !IsJavaScriptCode(word) &&
+                        !IsNonContentText(word) &&
+                        !stopWords.Contains(word)
+                    )
                     .ToArray();
 
-                foreach (string word in words)
+                var wordCount = words
+                    .GroupBy(word => word)
+                    .ToDictionary(group => group.Key, group => group.Count());
+
+                var result = new WordCountResult
                 {
-                    if (wordCount.ContainsKey(word))
-                        wordCount[word]++;
-                    else
-                        wordCount[word] = 1;
-                }
+                    TotalCount = wordCount.Sum(kv => kv.Value),
+                    WordCounts = wordCount.OrderByDescending(kv => kv.Value)
+                        .Select(kv => new WordCount { Word = kv.Key, Count = kv.Value })
+                        .ToList()
+                };
+
+                return result;
             }
-
-            var result = new WordCountResult
-            {
-                TotalCount = wordCount.Sum(kv => kv.Value),
-                WordCounts = wordCount.OrderByDescending(kv => kv.Value).Select(kv => new WordCount { Word = kv.Key, Count = kv.Value }).ToList()
-            };
-
-            return result;
         }
-    
+
+        private bool IsCssRelated(string word)
+        {
+            return Regex.IsMatch(word, @"^(webkit|moz|ms|o)-[a-z]+(-[a-z]+)?$");
+        }
+
+        private bool IsJavaScriptCode(string word)
+        {
+            return word.Equals("function") || word.Equals("var") || word.Equals("const") || word.Equals("let");
+        }
+
+        private bool IsNonContentText(string word)
+        {
+            return word.Length <= 3;
+        }
     }
 }
